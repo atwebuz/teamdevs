edb6b295c9a81c078ad106674220ecff04608c64
Your Name <you@example.com>
2022-12-28 22:17:20 +0500

    firstCommit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <div class="header_content d-flex flex-column align-items-center text-center h-100 w-100 justify-content-center">
       <h1 class="title animate__animated animate__bounce">Нам важны ваши цели!</h1>
       <button class="btn_modal" type="button">
-        <span class="btn-text">Бесплатная консультация</span>
+        <span class="btn-text">Бесплатная консультация !!!</span>
       </button>
     </div>
   </section>
